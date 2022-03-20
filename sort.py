7246e12c394f4eef97a3d397850457f0ae2a5acb
ArtavazdGevorgyan <ag15012003@gmail.com>
2022-03-21 01:59:27 +0400

    Update sort.py

--- sort.py
+++ sort.py
@@ -2,13 +2,13 @@
 # Bracking file to smaller ones 
 #########################
 
+
 with open("inputfile.txt")as fileinput:
     chunk_size = 125
     for (i, line) in enumerate(fileinput):
         numfiles=int(i/chunk_size)
         with open(f"file{int(i/chunk_size)}.txt", "a") as fileout:
             fileout.write(line)
-        lastline_no=i+1
 
 
 #########################
@@ -17,18 +17,21 @@ with open("inputfile.txt")as fileinput:
 
 for num in range(0,numfiles+1):
     values=dict()
+    elem=0
     with open(f"file{num}.txt", "r") as file:
         for line in file:
             line = line.strip(' \n')
             lst = list(line.split(" "))
             for i in lst:
-                try: values[int(i)] += 1
-                except: values[int(i)] = 1
+                if int(i) in values.keys():
+                    values[int(i)] += 1
+                    elem+=1
+                else:
+                    values[int(i)] = 1
+                    elem+=1
     with open(f"file{num}.txt", "w") as file:
         item,number =0,0
-        k=125*32;                                       # fileum gtnvox 
-        if num==numfiles: k=lastline_no%125*32        # elementneri qanak
-        while item<k:
+        while item<elem:
             if number in values.keys():
                 for j in range(0, values[number]):
                     file.write(f"{str(number)}\n")
