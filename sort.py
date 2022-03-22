6b50c04143b1c82df8b9cdfce60f8605110dce1d
ArtavazdGevorgyan <ag15012003@gmail.com>
2022-03-23 01:21:15 +0400

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,23 +1,25 @@
+import os
+
 #########################
 # Bracking file to smaller ones 
 #########################
 
 
 with open("inputfile.txt")as fileinput:
-    chunk_size = 125
+    numfiles=0
     for (i, line) in enumerate(fileinput):
-        numfiles=int(i/chunk_size)
-        with open(f"file{int(i/chunk_size)}.txt", "a") as fileout:
+        with open(f"file{numfiles}.txt", "a") as fileout:
             fileout.write(line)
+            if os.path.getsize(f"file{numfiles}.txt")>200_000_000:
+                numfiles+=1
 
 
 #########################
 # Sorting each file 
 #########################
-
+elem=[0 for i in range(0,numfiles+1)]
 for num in range(0,numfiles+1):
     values=dict()
-    elem=0
     with open(f"file{num}.txt", "r") as file:
         for line in file:
             line = line.strip(' \n')
@@ -25,13 +27,13 @@ for num in range(0,numfiles+1):
             for i in lst:
                 if int(i) in values.keys():
                     values[int(i)] += 1
-                    elem+=1
+                    elem[num]+=1
                 else:
                     values[int(i)] = 1
-                    elem+=1
+                    elem[num]+=1
     with open(f"file{num}.txt", "w") as file:
         item,number =0,0
-        while item<elem:
+        while item<elem[num]:
             if number in values.keys():
                 for j in range(0, values[number]):
                     file.write(f"{str(number)}\n")
@@ -42,3 +44,25 @@ for num in range(0,numfiles+1):
 #########################
 # Merging files together 
 #########################
+
+
+lst=[None for i in range(0,numfiles+1)]
+curindex=[0 for i in range(0,numfiles+1)]
+for num in range(0,numfiles+1):
+    with open(f"file{num}.txt", "r") as f:
+        lst[num]=int(f.readline().strip(" \n"))
+with open("outfile.txt","w") as file:
+    while True:
+        i=lst.index(min(lst))
+        file.write(f"{lst[i]}\n")
+        curindex[i]+=1
+        if curindex[i]>=elem[i]:
+            lst[i]=10000001                        # biggest element in file
+        
+        else:
+            with open(f"file{i}.txt","r") as ff:
+                a=ff.read().split('\n')
+                lst[i]=int(a[curindex[i]])
+        if  [10000001]*len(lst) == lst:
+            break
+
