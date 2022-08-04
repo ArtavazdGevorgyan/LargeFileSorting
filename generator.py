bd3b1db69f3989a66e66f52d47c89186ddc61149
ArtavazdGevorgyan <ag15012003@gmail.com>
2022-08-04 11:08:37 +0400

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,8 +1,9 @@
 import random
 import os
 
+# Generatin 4 GB file
 with open("inputfile.txt", "w") as file:
-    while os.path.getsize("inputfile.txt")<4_000_000_000:
+    while os.path.getsize("inputfile.txt") < 4_000_000_000:
         for i in range(0, 32):
             file.write(f"{str(random.randint(0, 9_999_999))} ")
         file.write("\n")
